2845b22169892aec08b62138a0c68b36081c2ef2
Aim-Entity <59455267+Aim-Entity@users.noreply.github.com>
2021-08-30 13:14:34 +0100

    Categories added

--- fleet/urls.py
+++ fleet/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.FleetAllListView.as_view(), name="fleet"),
+    path("first-class/", views.FleetFirstClassListView.as_view(), name="first-class"),
+    path("executive/", views.FleetExecutiveListView.as_view(), name="executive"),
+    path("business/", views.FleetBusinessListView.as_view(), name="business"),
+    path("xl/", views.FleetLargeListView.as_view(), name="xl"),
     path("item/<str:slug>", views.FleetAllDetailView.as_view(), name="fleet-detail")
 ]
--- fleet/views.py
+++ fleet/views.py
@@ -22,6 +22,54 @@ class FleetAllListView(ListView):
         return context
 
 
+class FleetFirstClassListView(ListView):
+    template_name = "fleet/fleet_first_class.html"
+    queryset = Car.objects.filter(category="First Class")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = CarFilter(
+            self.request.GET, queryset=self.get_queryset())
+
+        return context
+
+
+class FleetExecutiveListView(ListView):
+    template_name = "fleet/fleet_executive.html"
+    queryset = Car.objects.filter(category="Executive")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = CarFilter(
+            self.request.GET, queryset=self.get_queryset())
+
+        return context
+
+
+class FleetBusinessListView(ListView):
+    template_name = "fleet/fleet_business.html"
+    queryset = Car.objects.filter(category="Business")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = CarFilter(
+            self.request.GET, queryset=self.get_queryset())
+
+        return context
+
+
+class FleetLargeListView(ListView):
+    template_name = "fleet/XL.html"
+    queryset = Car.objects.filter(category="XL")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = CarFilter(
+            self.request.GET, queryset=self.get_queryset())
+
+        return context
+
+
 class FleetAllDetailView(DetailView):
     template_name = "fleet/fleet_detail.html"
     queryset = Car.objects.all()
